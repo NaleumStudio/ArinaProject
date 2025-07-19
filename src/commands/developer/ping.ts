b3fb87bb5c529ef2612bde9b635bf1787f45466a
DevNyanHa <lis131017@gmail.com>
2025-07-19 15:57:00 +0900

    Progress Bar Remake

--- src/commands/developer/ping.ts
+++ src/commands/developer/ping.ts
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder, EmbedBuilder, ChatInputCommandInteraction } from 'discord.js';
-import { EMOJI } from '../../utils/libs';
+import { EMOJI, getProgressBar } from '../../utils/libs';
 
 export const data = new SlashCommandBuilder()
     .setName('핑')
@@ -8,7 +8,7 @@ export const data = new SlashCommandBuilder()
 export async function execute(interaction: ChatInputCommandInteraction) {
     const initialEmbed = new EmbedBuilder()
         .setColor(0xF49ABA)
-        .setTitle(`${EMOJI.EM002} 핑!`)
+        .setTitle(`${EMOJI.EM0002} 핑!`)
         .setDescription('탁구공을 던지는 중...');
 
     await interaction.reply({ embeds: [initialEmbed] });
@@ -18,37 +18,22 @@ export async function execute(interaction: ChatInputCommandInteraction) {
     const apiLatency = interaction.client.ws.ping >= 0 ? `${interaction.client.ws.ping}ms` : '측정 불가';
 
     const memoryUsageMB = process.memoryUsage().heapUsed / 1024 / 1024;
-    const memoryUsagePercent = (
+    const memoryUsagePercent = +(
         (memoryUsageMB / (process.memoryUsage().heapTotal / 1024 / 1024)) *
         100
     ).toFixed(0);
 
-    const getProgressBar = (percentStr: string) => {
-        const p = Number(percentStr);
-        const fullBlocks = Math.floor(p / 10);
-        const remainder = Math.floor(p % 10);
-
-        let colorBlock = '⬜';
-        if (remainder <= 1) colorBlock = '⬜';
-        else if (remainder <= 5) colorBlock = '🟨';
-        else if (remainder <= 7) colorBlock = '🟧';
-        else colorBlock = '🟥';
-
-        return '🟩'.repeat(fullBlocks) + (fullBlocks < 10 ? colorBlock : '') + '⬜'.repeat(10 - fullBlocks - 1);
-    };
-
-    // 두번째 응답: 핑 결과 메시지
     const finalEmbed = new EmbedBuilder()
         .setColor(0xF49ABA)
-        .setTitle(`${EMOJI.EM002} 퐁!`)
+        .setTitle(`${EMOJI.EM0002} 퐁!`)
         .addFields(
         { name: 'PING', value: `\`${latency}ms\``, inline: true },
         { name: 'API', value: `\`${apiLatency}\``, inline: true },
         {
             name: 'MEMORY',
-            value: `\`${memoryUsageMB.toFixed(2)}MB\`\n\`${getProgressBar(memoryUsagePercent)}\``,
+            value: `\`${memoryUsageMB.toFixed(2)}MB\`\n${getProgressBar(memoryUsagePercent)}`,
             inline: false,
-        },
+        }
         )
         .setTimestamp();
 
--- src/utils/libs.ts
+++ src/utils/libs.ts
@@ -10,6 +10,34 @@ export type EnvKeys = keyof typeof ENV;
 export type EnvValues = (typeof ENV)[EnvKeys];
 
 export const EMOJI = {
-    EM001: '<:EM0001:1395218716547088576>',
-    EM002: '<:EM002:1395231073314537543>',
+    EM0001: '<:EM0001:1395218716547088576>',
+    EM0002: '<:EM0002:1395231073314537543>',
+    EM0003: '<:EM0003:1396008672177422468>', //bar
+    EM0004: '<:EM0004:1396008686048247879>',
+    EM0005: '<:EM0005:1396008700988231730>',
+    EM0006: '<:EM0006:1396008721166893218>',
+    EM0007: '<:EM0007:1396017836421415004>',
+    EM0008: '<:EM0008:1396008758513238116>',
+    EM0009: '<:EM0009:1396008773738299444>',
+    EM0010: '<:EM0010:1396022408741130240>',
 } as const;
+
+export function getProgressBar(p: number) {
+    p = Math.min(Math.max(p, 0), 100);
+    const pa = Math.round(p / 10);
+
+    let pb: string;
+
+    switch (pa) {
+        case 0:
+            pb = EMOJI.EM0006+EMOJI.EM0007.repeat(8)+EMOJI.EM0008; break;
+        case 1:
+            pb = EMOJI.EM0010+EMOJI.EM0007.repeat(8)+EMOJI.EM0008; break;
+        case 10:
+            pb = EMOJI.EM0003+EMOJI.EM0004.repeat(8)+EMOJI.EM0005; break;
+        default:
+            pb = EMOJI.EM0003+EMOJI.EM0004.repeat(pa - 2)+EMOJI.EM0009+EMOJI.EM0007.repeat(9 - pa)+EMOJI.EM0008;
+    }
+
+    return pb;
+}
